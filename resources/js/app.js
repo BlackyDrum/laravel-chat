7fec1f774f8dfdcdc3ee95b7c766e87e76a2a5ef
BlackyDrum <ganiaytan1010@web.de>
2023-09-06 00:11:47 +0200

    Primevue setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,9 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
+import "primevue/resources/themes/lara-light-indigo/theme.css";
+import PrimeVue from 'primevue/config';
+
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
@@ -15,6 +18,7 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue, Ziggy)
+            .use(PrimeVue)
             .mount(el);
     },
     progress: {
