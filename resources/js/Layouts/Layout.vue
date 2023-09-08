4921d9461d571ad5a36a3a2ada9559548d161589
BlackyDrum <ganiaytan1010@web.de>
2023-09-08 18:31:54 +0200

    Add Homepage and Page Layout

---
+++ resources/js/Layouts/Layout.vue
@@ -0,0 +1,10 @@
+<script setup>
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <div class="min-h-screen dark:bg-gray-900">
+        <slot/>
+    </div>
+</template>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { Head } from '@inertiajs/vue3';
+import Layout from "@/Layouts/Layout.vue";
+
+</script>
+
+<template>
+    <Head title="Home" />
+
+    <Layout>
+
+    </Layout>
+</template>
