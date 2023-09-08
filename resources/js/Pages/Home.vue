dcd3334cc3e3f5581b624ba3ff3bb4d950ce8da2
BlackyDrum <ganiaytan1010@web.de>
2023-09-09 00:51:40 +0200

    Add ``Primeicons`` package and input field for messages

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,7 +1,18 @@
 <script setup>
-import { Head } from '@inertiajs/vue3';
+import { Head, Link } from '@inertiajs/vue3';
+import {ref} from "vue";
+
 import Layout from "@/Layouts/Layout.vue";
 
+import ScrollPanel from 'primevue/scrollpanel';
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
+
+const message = ref(null);
+
+const sendMessage = () => {
+    message.value = null;
+}
 </script>
 
 <template>
@@ -10,14 +21,17 @@ import Layout from "@/Layouts/Layout.vue";
     <Layout>
         <div class="grid grid-cols-[15%,85%] min-h-screen">
             <div class="border-r border-gray-400">
-                
-            </div>
-            <div class="m-auto">
-                <div v-if="!$page.props.auth.user">
-                    Please Login to access the Chat
-                </div>
-                <div v-else>
 
+            </div>
+            <div class="m-auto" v-if="!$page.props.auth.user">
+                Please Login to access the Chat
+            </div>
+            <div class="grid grid-rows-[88%,12%]" v-else>
+                <ScrollPanel class="h-[90%] w-[95%] self-center mx-auto bg-gray-800 rounded-lg">
+
+                </ScrollPanel>
+                <div class="flex">
+                    <InputText class="w-[95%] h-16 mx-auto bg-gray-800 rounded-lg" v-model="message" @keydown.enter="sendMessage" placeholder="Type a Message"/>
                 </div>
             </div>
         </div>
--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,7 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
 import "primevue/resources/themes/lara-light-indigo/theme.css";
+import 'primeicons/primeicons.css';
 import PrimeVue from 'primevue/config';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
