c0663893d968c2a8fc88ca46b97590d792fc9b6a
BlackyDrum <ganiaytan1010@web.de>
2023-09-24 23:10:06 +0200

    Add rate limiter for sending messages

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -6,6 +6,8 @@ use App\Events\MessageSent;
 use App\Models\Message;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\RateLimiter;
+use const http\Client\Curl\AUTH_ANY;
 
 class MessageController extends Controller
 {
@@ -15,11 +17,23 @@ class MessageController extends Controller
             'message' => 'required|string|max:512',
         ]);
 
-        Message::query()->create([
-            'message' => $request->input('message'),
-            'creator_id' => Auth::id()
-        ]);
+        $execute = RateLimiter::attempt(
+            'send-message'.Auth::id(),
+            10,
+            function() use ($request) {
+                Message::query()->create([
+                    'message' => $request->input('message'),
+                    'creator_id' => Auth::id()
+                ]);
 
-        event(new MessageSent());
+                event(new MessageSent());
+            },
+            10
+        );
+
+        if (!$execute)
+        {
+            abort(429, "Too many messages sent. Try again in ". RateLimiter::availableIn('send-message'.Auth::id())." seconds");
+        }
     }
 }
