08e9c21d2708a3755baba9e97cbfbe1312abdf7e
BlackyDrum <ganiaytan1010@web.de>
2023-09-26 22:19:49 +0200

    Add socialite client callback to .env file

--- config/services.php
+++ config/services.php
@@ -34,13 +34,13 @@ return [
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect' => 'http://localhost:8000/auth/github/callback',
+        'redirect' => env('GITHUB_CLIENT_CALLBACK'),
     ],
 
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://localhost:8000/auth/google/callback',
+        'redirect' => env('GOOGLE_CLIENT_CALLBACK'),
     ],
 
 ];
