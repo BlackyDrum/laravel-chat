199162e322946be28466148dc631fbbc6b13413c
BlackyDrum <ganiaytan1010@web.de>
2023-09-29 00:05:00 +0200

    Remove user from active room before logging out

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -48,6 +49,10 @@ class AuthenticatedSessionController extends Controller
      */
     public function destroy(Request $request): RedirectResponse
     {
+        User::query()->find(Auth::id())->update([
+            'room_id' => null,
+        ]);
+
         Auth::guard('web')->logout();
 
         $request->session()->invalidate();
